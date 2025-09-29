519d1ca504e30778298e42cb842a76def138fc27
Jaysinghgautam <gjaysingh53@gmail.com>
2025-09-29 16:38:08 +0530

    updated searchbar

--- Frontend/src/components/SearchBar.jsx
+++ Frontend/src/components/SearchBar.jsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import assets from '../assets/assets';
 import {useContext} from 'react'
 import { ShopContext } from '../context/ShopContext';
+import { useLocation } from 'react-router-dom';
 
 function SearchBar() {
 
     const {search,setSearch,showSearch,setShowSearch} = useContext(ShopContext);
+    const location = useLocation();
+    const [visible,setVisible] = useState(true);
 
-  return showSearch ? (
+    useEffect(()=> {
+        if(location.pathname.includes('collection') ) {
+            setVisible(true);
+        } else {
+            setVisible(false);
+        }
+    },[location])
+  return showSearch && visible ? (
     <div className="border-t border-b bg-gray-50 text-center">
         <div className=" inline-flex item-center justify-center border border-gray-400 px-5 py-2 my-5 mx-3 rounded-full w-3/4 sm:w-1/2 ">
         
